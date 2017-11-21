24b195150fc6da6afb3174d7c12ffdf55262a9f2
shivanir <shivanir@INshivanir.local>
2017-11-21 22:34:15 +0530

    Create basic migration script and add login page

--- web_app/settings.py
+++ web_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'user_file_uploader',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
