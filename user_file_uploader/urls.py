3efab817ff936fe64ef9622f040c24f54c9e9483
shivanir <shivanir@INshivanir.local>
2017-11-24 00:25:57 +0530

    Add user authentication

--- user_file_uploader/urls.py
+++ user_file_uploader/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^login/$', views.login),
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^login/$', auth_views.login, {'template_name' : 'login.html'}, name = 'login'),
 ]
--- user_file_uploader/views.py
+++ user_file_uploader/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-def login(request):
-    return render(request, 'user/login.html')
+@login_required(login_url="login/")
+def dashboard(request):
+    context = { 'user_name': request.user.username }
+    return render(request, 'dashboard.html', context)
